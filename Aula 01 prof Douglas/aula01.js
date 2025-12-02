410616eaa960c3a4ec90c6d62363136f64117fa2
Rodrigo Azevedo <rodrilek.azevedo@gmail.com>
2025-12-01 21:16:22 -0300

    feat:commit3

---
+++ Aula 01 prof Douglas/aula01.js
@@ -0,0 +1,23 @@
+// let nome= 'rodrigo';
+// let casa=284;
+// let nota=2.4;
+
+// console.log(typeof nome)
+// console.log(nome)
+// console.log(typeof casa)
+// console.log(typeof nota)
+
+
+// let x= 5;
+// let y= 5;
+
+// let soma= x+y
+// let subtraçao= x-y
+// let mult= x*y
+// let div= x/y
+
+// console.log(soma)
+// console.log(subtraçao)
+// console.log(div)
+// console.log(mult)
+console.log(5 === '5')
